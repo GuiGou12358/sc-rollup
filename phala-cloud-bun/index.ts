cd13900c6477dffb531c5e4c179e4ea5541f3a26
guigou <guigoucrypto@gmail.com>
2025-04-24 10:02:59 +0200

    fix issues with phala-cloud-bun

--- phala-cloud-bun/index.ts
+++ phala-cloud-bun/index.ts
@@ -2,14 +2,14 @@ import {serve} from "bun";
 import {TappdClient} from "@phala/dstack-sdk";
 import {Keyring} from "@polkadot/keyring";
 import type {KeyringPair} from "@polkadot/keyring/types";
-import type {InkClient} from "@guigou/sc-rollup-ink5";
+import {InkClient} from "@guigou/sc-rollup-ink5";
+import cron from "node-cron";
 
 const inkClientRpc = process.env.INK_CLIENT_RPC;
 const inkClientAddress = process.env.INK_CLIENT_ADDRESS;
 const pk = process.env.ATTESTOR_PK;
 const port = process.env.PORT || 3000;
 console.log(`Listening on port ${port}`);
-
 
 async function getSubstrateKeyringPair(client: TappdClient) : Promise<KeyringPair> {
   const result = await client.deriveKey('polkadot');
@@ -69,6 +69,61 @@ serve({
       return new Response(JSON.stringify(tx));
     },
 
+    "/poll_message": async (req) => {
+
+      const tx = await poolMessage();
+      return new Response(JSON.stringify(tx));
+    },
+
+    "/start_polling": async (req) => {
+
+      console.log('Message polling enabled');
+
+      const task = cron.schedule('*/5 * * * *',
+        async () => {
+          await poolMessage();
+        });
+      task.start();
+
+      return new Response(JSON.stringify({task}));
+    },
+
+    "/list_scheduled_tasks": async (req) => {
+
+      const tasks = cron.getTasks();
+      return new Response(JSON.stringify({tasks}));
+    },
+
+
   },
 });
+
+const poolMessage = async ()=> {
+
+
+  if (!inkClientRpc ||  !inkClientAddress || !pk){
+    console.log('Missing configuration!');
+  }
+
+  console.log('Poll message ...');
+
+  const inkClient = new InkClient(inkClientRpc, inkClientAddress, pk);
+  await inkClient.startSession();
+
+  const tail = await inkClient.getQueueTailIndex();
+  console.log('Tail Index: ' + tail);
+  const head = await inkClient.getQueueHeadIndex();
+  console.log('Head Index: ' + head);
+  const hasMessage = await inkClient.hasMessage();
+  console.log('hasMessage : ' + hasMessage);
+
+  let message;
+  do {
+    message = await inkClient.pollMessage();
+    console.log('message %s', message);
+  } while (message.isSome());
+
+  await inkClient.commit();
+}
+
 
